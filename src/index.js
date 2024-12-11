3e3f31d15d52dff0a497607368bcf99b41899540
Hassan <hassanmahad07@gmail.com>
2024-12-11 10:46:28 +0300

    Intial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {createRoot} from 'react-dom/client';
+let year = 2026;
+let greating = 
+<div className="box">
+    <h1 className="title"> Title </h1>
+    <h1 style={{
+        backgroundColor : 'green',
+        padding : 10,
+        color : 'white',
+        borderRadius : 10,
+        border : '1px solid black',
+        marginLeft : 10,
+        marginTop : 10,
+        fontFamily : 'Arial',
+        fontSize : 20,
+
+        }}>Hello From React {year}</h1>
+        <input type="text" placeholder='Enter Your Name'></input>
+</div>;
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(greating);
